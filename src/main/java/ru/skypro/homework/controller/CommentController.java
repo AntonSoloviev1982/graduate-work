acfda4ca20507ee9644073f7a39bc3ebe9c82d1a
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-10-22 03:04:17 +0300

    redone (all DTOs are written correctly and according to specification)

--- src/main/java/ru/skypro/homework/controller/CommentController.java
+++ src/main/java/ru/skypro/homework/controller/CommentController.java
@@ -2,11 +2,12 @@ package ru.skypro.homework.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.skypro.homework.dto.CommentDto;
-
-import java.util.List;
+import ru.skypro.homework.dto.Comment;
+import ru.skypro.homework.dto.Comments;
+import ru.skypro.homework.dto.CreateOrUpdateComment;
 
 @RestController
 @RequestMapping("/ads/{adId}/comments")
@@ -15,27 +16,27 @@ public class CommentController {
     private static final Logger logger = LoggerFactory.getLogger(CommentController.class);
 
     @GetMapping()
-    public ResponseEntity<?> getComments(@PathVariable int adId){
+    public ResponseEntity<Comments> getComments(@PathVariable Integer adId){
         logger.info("The get all ad comments method is called.");
-        return ResponseEntity.ok(List.of(new CommentDto()));
+        return ResponseEntity.ok(new Comments());
     }
 
-    @PostMapping("/{commentId}")
-    public ResponseEntity<?> addComment(@PathVariable int adId, @PathVariable int commentId, String text){
+    @PostMapping()
+    public ResponseEntity<Comment> addComment(@PathVariable Integer adId, @RequestBody CreateOrUpdateComment comment){
         logger.info("The comment creation method is called.");
-        return ResponseEntity.ok(new CommentDto());
+        return ResponseEntity.ok(new Comment());
     }
 
     @PatchMapping("/{commentId}")
-    public ResponseEntity<?> updateComment(@PathVariable int adId, @PathVariable int commentId, String text){
+    public ResponseEntity<Comment> updateComment(@PathVariable Integer adId, @PathVariable Integer commentId,@RequestBody CreateOrUpdateComment comment){
         logger.info("The comment update method is called.");
-        return ResponseEntity.ok(new CommentDto());
+        return ResponseEntity.ok(new Comment());
     }
 
     @DeleteMapping("/{commentId}")
-    public ResponseEntity<?> deleteComment(@PathVariable int adId, @PathVariable int commentId){
+    public ResponseEntity<?> deleteComment(@PathVariable Integer adId, @PathVariable Integer commentId){
         logger.info("The comment delete method is called.");
-        return ResponseEntity.ok(new CommentDto());
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 
 }
---
+++ src/main/java/ru/skypro/homework/dto/Comments.java
@@ -0,0 +1,14 @@
+package ru.skypro.homework.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Comments {
+
+    private Integer count;
+
+    private Comment[] results;
+
+}
