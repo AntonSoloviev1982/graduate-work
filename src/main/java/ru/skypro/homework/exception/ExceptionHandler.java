fbd4cfe2dcd990aca305323b737ca25f3c5cb1d4
Anton <Soloviev.gvardiya@gmail.com>
2023-10-23 17:44:00 +0300

    Исправил замечания Димы по валидации и методу setAvatar.

--- src/main/java/ru/skypro/homework/exception/ExceptionHandler.java
+++ src/main/java/ru/skypro/homework/exception/ExceptionHandler.java
@@ -4,16 +4,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 
 @RestControllerAdvice
 public class ExceptionHandler {
     private final Logger logger = LoggerFactory.getLogger(ExceptionHandler.class);
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(ValidationException.class)
-    public ResponseEntity<?> handlerValidationError(ValidationException e) {
-        logger.error("Переданный в запросе json не валиден, ошибки валидации: {}", e.getViolations());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getViolations());
+    @org.springframework.web.bind.annotation.ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> handlerValidationError(ConstraintViolationException e) {
+        String resultValidations = e.getConstraintViolations().stream()
+                        .map(ConstraintViolation::getMessage)
+                                .reduce((s1, s2) -> s1 + ". " + s2).orElse("");
+        logger.error("Переданный в запросе json не валиден, ошибки валидации: {}", resultValidations);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
 }
