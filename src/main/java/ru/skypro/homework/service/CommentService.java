7a79f4f1b6a63066e02485f97a3923fe4d057b16
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-11-23 02:40:55 +0300

    JavaDoc

--- src/main/java/ru/skypro/homework/service/CommentService.java
+++ src/main/java/ru/skypro/homework/service/CommentService.java
@@ -2,6 +2,7 @@ package ru.skypro.homework.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.skypro.homework.dto.Comment;
@@ -17,6 +18,11 @@ import ru.skypro.homework.repository.UserRepository;
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
 
+/**
+ *  Сервис "CommentService".
+ *  Cоздает, редактирует и удаляет комментарии к объявлениям т.е. записи в БД в таблице "ad_comment".<br>
+ *  Находит список всех комментариев к данному объявлению.
+ */
 @Service
 public class CommentService {
 
@@ -33,6 +39,14 @@ public class CommentService {
         this.userRepository = userRepository;
     }
 
+    /**
+     *  Cоздает запись в БД в таблице "ad_comment".<br>
+     *  Используется метод репозитория {@link JpaRepository#save(Object)}
+     *  @param adId идентификатор объявления
+     *  @param createOrUpdateComment DTO,содержащее текст комментария
+     *  @param userName имя автора комментария
+     *  @return возвращает полную информацию о коментарии (текст, когда был создан, кем и к какому объявлению)
+     * */
     @Transactional
     public Comment createComment(Integer adId,
                                  CreateOrUpdateComment createOrUpdateComment,
@@ -43,12 +57,28 @@ public class CommentService {
                         commentMapper.toEntity(createOrUpdateComment, adId, getCurrentUser(userName).getId())));
     }
 
+    /**
+     *  Находит список всех комментариев к данному объявлению в БД в таблице "ad_comment".<br>
+     *  Используется метод репозитория {@link JpaRepository#findAll()}
+     *  @param adId идентификатор объявления
+     *  @return возвращает список всех комментариев к данному объявлению
+     * */
     @Transactional
     public Comments findComments(Integer adId){
         LOGGER.info("Find all comments.");
         return  commentMapper.toComments(commentRepository.findAllByAdId(adId));
     }
 
+    /**
+     *  Изменяет запись в БД в таблице "ad_comment".<br>
+     *  Используется метод репозитория {@link JpaRepository#save(Object)}
+     *  @param adId идентификатор объявления
+     *  @param commentId идентификатор редактируемого комментария
+     *  @param createOrUpdateComment DTO,содержащее текст комментария
+     *  @param userName имя автора комментария
+     *  @throws EntityNotFoundException если объявления или комментария с указанными идентификаторами нет в БД.
+     *  @return возвращает полную информацию о коментарии (текст, когда был создан, кем и к какому объявлению)
+     * */
     @Transactional
     public Comment updateComment(Integer adId,
                                  Integer commentId,
@@ -66,6 +96,13 @@ public class CommentService {
         return commentMapper.toDto(commentRepository.save(oldComment));
     }
 
+    /**
+     *  Удаляет запись из БД в таблице "ad_comment".<br>
+     *  Используется метод репозитория {@link JpaRepository#delete(Object)}
+     *  @param adId идентификатор объявления
+     *  @param commentId идентификатор удаляемого комментария
+     *  @throws EntityNotFoundException если объявления или комментария с указанными идентификаторами нет в БД.
+     * */
     @Transactional
     public void deleteComment(Integer adId, Integer commentId){
         LOGGER.info("Delete comment.");
